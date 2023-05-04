e5810399ec25343fb1b3f098de1fa2faf6d33682
pirate-zip <nathendorsing@gmail.com>
2023-05-04 16:32:00 -0500

    worked on error 404 screen

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,31 @@
 import Link from 'next/link';
 import styles from '@/styles/Home.module.css'
+import Head from 'next/head'
+import Image from 'next/image'
 
 const NotFoundPage = () => {
   return <>
-    <div className={styles.errorhead}>
-      <h1>404 - Page Not Found</h1>
-      </div>
+  <Head>
+<title>Error 404 - Page Not Found</title>
+<link rel="icon" href="/pirate-zip_pfp.ico"/>
+</Head>
+    <div1 className={styles.errorbkg}><div className={styles.errorhead}>
+      <h1>Error 404 - Page Not Found</h1>
+      {/* </div> */}
       <a href='/' target='_self'>
-      <div className={styles.errorsub}>return to main</div>
-      <link></link>
-    </a>
+      <div className={styles.errorsub}>return to main
+</div>
+    </a></div>
+    <div className={styles.errorpic}><Image
+                src="/pirate-zip_pfp.png"
+                alt="pirate-zip's pfp"
+                className={styles.vercelLogo}
+                width={100}
+                height={100}
+                priority
+              />
+</div>
+</div1>
 </>
 }
 
